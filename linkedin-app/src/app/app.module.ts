48e480012fb205de83f3ac5c87cc9d17efdb7513
Farah Mahmoud <45809462+fr7m7moud70@users.noreply.github.com>
2020-03-19 14:55:56 +0200

    creating folders and main component

--- linkedin-app/src/app/app.module.ts
+++ linkedin-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './cores/header/header.component';
+import { ErrorPageComponent } from './cores/error-page/error-page.component';
+import { FooterComponent } from './cores/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { ProfileComponent } from './features/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ErrorPageComponent,
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
